444ed447466389bd2569f04bd4facd160bda44e8
Gerald Schneider <gerald@schneidr.de>
2023-08-15 07:31:10 +0200

    switched to py-staticmaps library

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - https://towardsdatascience.com/simple-gps-data-visualization-using-python-and-open-street-maps-50f992e9b676
 - https://pypi.org/project/gpxpy/
+- https://github.com/flopp/py-staticmaps
--- gpxvideo.py
+++ gpxvideo.py
@@ -3,6 +3,7 @@ from gps_class import GPSVis
 import gpxpy
 import gpxpy.gpx
 from PIL import Image, ImageDraw
+import staticmaps
 import sys
 import tempfile
 
@@ -22,13 +23,13 @@ parser.add_argument('--fps', type=int,
                     default=30)
 parser.add_argument('--trackwidth', type=int,
                     help='Width of the track',
-                    default=8)
+                    default=10)
 parser.add_argument('--trackcolor', choices=['red', 'green', 'blue'],
                     help='Color of the track',
                     default='blue')
 parser.add_argument('--maptype', choices=['transparent', 'osm'],
                     help='Map style to use for the background',
-                    default='transparent')
+                    default='osm')
 args = parser.parse_args()
 
 def scale_to_img(lat_lon, startpos, h_w, points):
@@ -57,45 +58,13 @@ gpx = gpxpy.parse(gpx_file)
 tmpdirname = tempfile.TemporaryDirectory(prefix='gpx', suffix=args.gpxfile.name)
 print(tmpdirname)
 
-latitude_max = -99
-latitude_min = 99
-longitude_max = -99
-longitude_min = 99
-
-for track in gpx.tracks:
-    for segment in track.segments:
-        for point in segment.points:
-            if point.latitude > latitude_max:
-                latitude_max = point.latitude
-            if point.latitude < latitude_min:
-                latitude_min = point.latitude
-            if point.longitude > longitude_max:
-                longitude_max = point.longitude
-            if point.longitude < longitude_min:
-                longitude_min = point.longitude
-
-latitude_width = latitude_max - latitude_min
-longitude_width = longitude_max - longitude_min
-
-print((latitude_width, longitude_width))
-if latitude_width < longitude_width:
-    track_width = args.width * 0.9
-    track_height = args.height * ((latitude_width*2)/longitude_width) * 0.9
-    track_x = (args.width - track_width) / 2
-    track_y = (args.height - track_height) / 2
-else:
-    track_height = args.height * 0.9
-    track_width = args.width * (longitude_width/(latitude_width*2)) * 0.9
-    track_x = (args.width - track_width) / 2
-    track_y = (args.height - track_height) / 2
-print ((track_width, track_height))
-
-points = (latitude_min, longitude_min, latitude_max, longitude_max)
+context = staticmaps.Context()
 
 if args.maptype == 'transparent':
-    background_image = Image.new("RGBA",(args.width,args.height), (0,0,0,0))
+    # background_image = Image.new("RGBA",(args.width,args.height), (0,0,0,0))
+    sys.exit('--maptype transparent not implemented yet')
 elif args.maptype == 'osm':
-    sys.exit('not implemented yet')
+    context.set_tile_provider(staticmaps.tile_provider_OSM)
 else:
     sys.exit('invalid maptype')
 
@@ -108,17 +77,10 @@ elif args.trackcolor == 'blue':
 else:
     sys.exit('invalid trackcolor')
 
-img_points = []
-
 for track in gpx.tracks:
     for segment in track.segments:
-        for point in segment.points:
-            x1, y1 = scale_to_img((point.latitude, point.longitude), (track_x, track_y), (track_width, track_height), points)
-            img_points.append((x1, y1))
-            # print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
+        line = [staticmaps.create_latlng(p.latitude, p.longitude) for p in segment.points]
+        context.add_object(staticmaps.Line(line))
 
-color=(0, 0, 255)
-draw = ImageDraw.Draw(background_image)
-draw.line(img_points, fill=color, width=args.trackwidth)
-
-background_image.save('test.png', 'PNG')
+image = context.render_cairo(args.width, args.height)
+image.write_to_png("test.png")
